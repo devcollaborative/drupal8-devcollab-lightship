ac926b635532c099209a5e50156db5985b606c99
Candice Dexter <candice.n.dexter@gmail.com>
2019-03-25 00:00:31 -0400

    update to include sourcemaps, update breakpoint

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
 // DevCollab Starter Theme
-// https://css-tricks.com/gulp-for-beginners/ 
+// https://css-tricks.com/gulp-for-beginners/
 
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
-// Delete generated files when needed 
+// Create sass sourcemaps
+var sourcemaps = require('gulp-sourcemaps');
+// Delete generated files when needed
 var del = require('del');
 
+var sass_config = {
+  includePaths: [
+    'node_modules/breakpoint-sass/stylesheets/',
+  ]
+};
+
 gulp.task('sass', function(){
-  return gulp.src('sass/*.scss')
+  return gulp.src('sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sass_config).on('error', sass.logError))
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('clean:css', function() {
+gulp.task('clean:css', function(){
   return del.sync('css/*');
-})
+});
 
 gulp.task('watch', function(){
-  gulp.watch('sass/*/*.scss', ['sass']); 
-  // Other watchers
-})
+  gulp.watch('sass/**/*.scss', ['sass']);
+});
