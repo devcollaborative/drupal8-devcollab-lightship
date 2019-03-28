da3ec0efad11ae35867384540946b1c79ebb5b4c
Candice Dexter <candice.n.dexter@gmail.com>
2019-03-28 15:14:51 -0400

    update gulp file per breakpoint removal

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,9 @@ var sourcemaps = require('gulp-sourcemaps');
 // Delete generated files when needed
 var del = require('del');
 
-var sass_config = {
-  includePaths: [
-    'node_modules/breakpoint-sass/stylesheets/',
-  ]
-};
-
 gulp.task('sass', function(){
   return gulp.src('sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass(sass_config).on('error', sass.logError))
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'));
