c607be3b1091a91999eaba2185f2bd2dd36762b6
Jeff Hipp <jeff@devcollaborative.com>
2020-03-24 17:34:45 -0400

    Linted custom.js, take 2

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,10 @@
  * Code for custom js.
  */
 
-(function ($, Drupal, drupalSettings) {
+(function($, Drupal, drupalSettings) {
   Drupal.behaviors.lightship = {
     attach(context, settings) {
-
       // Scripts go here.
-    },
+    }
   };
-}(jQuery, Drupal, drupalSettings));
+})(jQuery, Drupal, drupalSettings);
