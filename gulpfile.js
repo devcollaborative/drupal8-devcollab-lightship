46edc34311bcbc673793b8d36922ffcb226586fe
Jeff Hipp <jeff@jeffhipp.com>
2020-05-26 13:27:39 -0400

    More linting fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,32 @@
  */
 
 // https://css-tricks.com/gulp-for-beginners/
-const gulp = require("gulp");
+const gulp = require('gulp');
 // Requires the gulp-sass plugin.
-const sass = require("gulp-sass");
+const sass = require('gulp-sass');
 // Create sass sourcemaps.
-const sourcemaps = require("gulp-sourcemaps");
+const sourcemaps = require('gulp-sourcemaps');
 // Delete generated files when needed.
-const del = require("del");
+const del = require('del');
 // Run a list of tasks in order.
-const runSequence = require("run-sequence");
+const runSequence = require('run-sequence');
 
-gulp.task("sass", () =>
+gulp.task('sass', () =>
   gulp
-    .src("sass/**/*.scss")
+    .src('sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass()) // Converts Sass to CSS with gulp-sass.
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest('css')),
 );
 
-gulp.task("clean:css", () => del.sync("css/*"));
+gulp.task('clean:css', () => del.sync('css/*'));
 
-gulp.task("watch", () => {
-  gulp.watch("sass/**/*.scss", ["sass"]);
+gulp.task('watch', () => {
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
 
 // One time build process.
-gulp.task("build", () => {
-  runSequence("clean:css", "sass");
+gulp.task('build', () => {
+  runSequence('clean:css', 'sass');
 });
