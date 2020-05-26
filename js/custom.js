25ceac8234e67cee5d347de78d397dec8539a4a9
Jeff Hipp <jeff@jeffhipp.com>
2020-05-26 12:39:59 -0400

    Eslint setup

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-unused-vars */
 /**
  * @file
  * Code for custom js.
  */
 
-(function($, Drupal, drupalSettings) {
+// eslint-disable-next-line func-names
+(function ($, Drupal, drupalSettings) {
   Drupal.behaviors.lightship = {
     attach(context, settings) {
       // Scripts go here.
-    }
+    },
   };
 })(jQuery, Drupal, drupalSettings);
